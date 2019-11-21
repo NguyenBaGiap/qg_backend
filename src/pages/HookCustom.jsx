9974e1a406a69609852398420a7c28f31b3796a5
Giap Nguyen Ba <giap_nb@septeni-technology.jp>
2019-11-21 11:25:48 +0700

    hook

--- src/pages/HookCustom.jsx
+++ src/pages/HookCustom.jsx
@@ -1,35 +1,37 @@
 import React, { useState, useEffect, useRef  } from 'react';
 import PropTypes from "prop-types";
 
-function searchGithub(query) {
-    return fetch(
-        `https://api.github.com/search/users?q=${query}+repos:%3E42+followers:%3E1000`
-    )
-        .then(resp => resp.json())
-        .then(resp => resp.items);
-}
-
-function useGitHubSearch(search) {
-    const [data, setData] = useState([]);
-
-    useEffect(
-        () => {
-            searchGithub(search).then(resp => setData(resp));
-        },
-        [search]
-    );
-
-    return data;
-}
-
 
 function GitHubSearchFunction(props) {
-    const data = useGitHubSearch(props.search);
+    //custom hook
+    const  useGitHubSearch = (search) => {
+        const [data, setData] = useState([]);
+
+        useEffect(
+            () => {
+                searchGithub(search).then(resp => setData(resp));
+            },
+            [search]
+        );
+
+        return data;
+    }
+
+    //fetch data
+    const  searchGithub = (query) => {
+        return fetch(
+            `https://api.github.com/search/users?q=${query}+repos:%3E42+followers:%3E1000`
+        )
+            .then(resp => resp.json())
+            .then(resp => resp.items);
+    }
+
+    const resultSearch = useGitHubSearch(props.search);
 
     return (
         <ul>
-            <li>Total result: {data.length} </li>
-            {data.map(item => (
+            <li>Total result: {resultSearch.length} </li>
+            {resultSearch.map(item => (
                 <li key={item.id}>
                     <a href={item.html_url}>
                         <img src={item.avatar_url} alt={item.name} />
