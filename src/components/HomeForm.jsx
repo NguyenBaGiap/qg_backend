81c9c60a091793906098c20a4a33005117ca79d4
Giap Nguyen Ba <giap_nb@septeni-technology.jp>
2020-09-04 10:30:37 +0700

    -a

--- src/components/HomeForm.jsx
+++ src/components/HomeForm.jsx
@@ -13,13 +13,29 @@ export default class HomeForm extends React.Component {
         })
         this.props.incrementNumber()
     }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log(prevProps)
+        console.log(prevState)
+        return prevState.value;
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+      if(prevState !== this.state){
+          console.log(snapshot)
+
+      } else {
+          //console.log("sdkfnsdkfnsjk")
+      }
+    }
+
     render() {
         console.log("render.....homeform")
 
         return (
           <div>
               <button onClick={this.incrementStateValue}>
-                  Tăng lên 1 đơn vị
+                  Tăng {this.state.value } lên 1 đơn vị
               </button>
           </div>
         );
