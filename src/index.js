360558e866eb0d599b3656eea24284afe2a9f317
Giap Nguyen Ba <giap_nb@septeni-technology.jp>
2020-09-09 15:07:25 +0700

    -a

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from 'react-router-dom';
-import ContentLayout from "./ContentLayout";
-import withRouterExample from "./Router";
-
-
-const App = withRouterExample(ContentLayout)
-ReactDOM.render(
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>,
-document.getElementById("app-container")
-);
+console.log("ahihi")
